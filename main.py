174899c1ecdd0a7b1ad5f39e241627436d5cef94
sirkotal <lordkotal2002@gmail.com>
2024-04-30 09:54:08 +0100

    baggins

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 # Load the data
 data = pd.read_csv('nasa.csv')
+data.fillna("NA", inplace=True)
 print(data.head())
+stats = data.describe()
+print(stats)
+
+print(sb.pairplot(data.dropna(), hue='Hazardous'))
