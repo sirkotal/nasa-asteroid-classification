ee1554d0b00d1efd96aaf15ff91671ad1164d30f
sirkotal <lordkotal2002@gmail.com>
2024-05-14 10:51:47 +0100

    Early implementation of SVM (not running; error or delay?)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from dtrees import train_decision_trees
 from knn import train_knn
+from svm import train_svm
 
 
 def main():
@@ -9,12 +10,14 @@ def main():
     #    sys.exit(1)
 
     # algorithm = sys.argv[1]
-    algorithm = "knn"
+    algorithm = "svm"
 
     if algorithm == 'decision_trees':
         train_decision_trees()
     elif algorithm == 'knn':
         train_knn()
+    elif algorithm == 'svm':
+        train_svm()
     else:
         sys.exit(1)
 
