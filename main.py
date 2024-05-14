8f0ce782cc160ab47ce3e854b694e9122e7c28c8
sirkotal <lordkotal2002@gmail.com>
2024-05-14 10:06:35 +0100

    Created a plot for the Classification Matrix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import seaborn as sb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
 
 # Load the data
 data = pd.read_csv('nasa.csv')
@@ -17,12 +17,13 @@ print(data.head())
 nasa_class = data.pop('Hazardous')
 data.insert(0, 'Hazardous', nasa_class)
 
-data.drop(columns=['Close Approach Date','Orbit Determination Date'],inplace = True)
+data.drop(columns=['Close Approach Date', 'Orbit Determination Date'], inplace=True)
 
 if (data['Equinox'].nunique() == 1):
     data.drop(columns=['Equinox'], inplace=True)
 if (data['Orbiting Body'].nunique() == 1):
     data.drop(columns=['Orbiting Body'], inplace=True)
+
 print(data.dtypes)
 stats = data.describe()
 print(stats)
@@ -36,9 +37,9 @@ hazard = data['Hazardous']
 hazard = label_encoder.fit_transform(hazard)
 
 (training_inputs,
-testing_inputs,
-training_classes,
-testing_classes) = train_test_split(new_data, hazard, test_size=0.25, random_state=1)
+ testing_inputs,
+ training_classes,
+ testing_classes) = train_test_split(new_data, hazard, test_size=0.25, random_state=1)
 
 dt_class = DecisionTreeClassifier(random_state=42)
 dt_class.fit(training_inputs, training_classes)
@@ -47,5 +48,15 @@ dt_class.score(testing_inputs, testing_classes)
 
 accuracy_score(testing_classes, dt_class.predict(testing_inputs))
 
-print(classification_report(testing_classes, dt_class.predict(testing_inputs)))
 print(confusion_matrix(testing_classes, dt_class.predict(testing_inputs)))
+
+cm_display = ConfusionMatrixDisplay(
+    confusion_matrix=confusion_matrix(testing_classes, dt_class.predict(testing_inputs)))
+cm_display.plot()
+plt.xticks([0, 1], ["False", "True"])
+plt.yticks([0, 1], ["False", "True"])
+plt.xlabel('Predicted Hazard')
+plt.ylabel('Actual Hazard')
+plt.show()
+
+print(classification_report(testing_classes, dt_class.predict(testing_inputs)))
